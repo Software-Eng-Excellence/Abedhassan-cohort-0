c78fcaa6bfee9ea1fd46844ecd1ca36539db1d34
AbedalrhmanAlhassan <habed3759@gmail.com>
2025-03-11 11:57:54 +0200

    all changes

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,18 @@
+import path from 'path';
+import {parseCSV} from './util/parser'
+import logger from './util/logger';
 
-import { FinanceCalculator, ItemValidator, MaxPriceValidator, OrderManagement, PriceValidator, Validator } from './app';
-import  logger  from './util/logger';
+const filePath = path.resolve(__dirname, './data/Cake orders.csv');
 
-const orders = [
-  { id: 1, item: "Sponge", price: 15 },
-  { id: 2, item: "Chocolate", price: 20 },
-  { id: 3, item: "Fruit", price: 18 },
-  { id: 4, item: "Red Velvet", price: 25 },
-  { id: 5, item: "Coffee", price: 8 },
-];
-
-const rules = [
-  new PriceValidator(),
-  new ItemValidator(),
-  new MaxPriceValidator()
-];
-
-const OrderManager = new OrderManagement(new Validator([...rules]), new FinanceCalculator());
-for (const order of orders) {
-  OrderManager.addOrder(order.item, order.price);
+async function main() {
+    try {
+        const products = await parseCSV(filePath)
+        for (const product of products) {
+            logger.info(product + '\n');
+        }
+    } catch(error) {
+        logger.error(error)
+    }
 }
 
-// Adding a new order directly
-const newItem = "Marble";
-const newPrice = 22;
-
-OrderManager.addOrder(newItem, newPrice);
-
-logger.info("Orders after adding a new order:", OrderManager.getOrders());
-
-// Calculate Total Revenue directly
-logger.info("Total Revenue:", OrderManager.getTotalRevenue());
-
-// Calculate Average Buy Power directly
-logger.info("Average Buy Power:", OrderManager.getBuyPower());
-
-// Fetching an order directly
-const fetchId = 2;
-const fetchedOrder = OrderManager.getOrder(fetchId);
-logger.info("Order with ID 2:", fetchedOrder);
-
-// Attempt to fetch a non-existent order
-const nonExistentId = 10;
-const nonExistentOrder = OrderManager.getOrder(nonExistentId);
-logger.info("Order with ID 10 (non-existent):", nonExistentOrder);
+main();
